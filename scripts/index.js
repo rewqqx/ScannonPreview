94cb26d1a9fdd42e9542e95f52ddfe7e3fdcf607
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-11-29 15:41:00 +0300

    Refactor Main

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,19 @@
 import {Background} from "./visual/Background.mjs";
 import {Scene} from './objects/Scene.mjs'
 import {Cannon} from './objects/implementations/Cannon.mjs'
-import {PlayerController} from "./controller/implementation/PlayerController.mjs";
 
-let background = document.getElementById("background");
-background.width = 1920;
-background.height = 1080;
-background.display = "";
-let bgContext = background.getContext("2d");
-let backgroundDrawer = new Background(bgContext);
-
-
-let canvas = document.getElementById("canvas");
-canvas.width = 1920;
-canvas.height = 1080;
-let context = canvas.getContext("2d");
-canvas.style.display = "none";
-
-
-var f = new FontFace('FredokaOne', 'url(./fonts/FredokaOne-Regular.ttf)');
-
-f.load().then(function(font) {
-
-    // Ready to use the font in a canvas context
-    console.log('Font ready: FredokaOne');
-
-    // Add font on the html page
-    document.fonts.add(font);
-
-    context.font = '48px FredokaOne';
-});
-
-
-let scene = new Scene(context);
-let cannon = new Cannon(context, scene, 75, 650);
-cannon.setController("player");
-
-
-scene.addItem(cannon);
 let gameStarted = false;
+
+loadFont();
+
+let background = initBackground();
+let scene = initScene();
+
 
 setInterval(tick, 10);
 
 function tick() {
-    backgroundDrawer.drawBackground();
+    background.drawBackground();
     if (gameStarted) {
         scene.tick();
     }
@@ -52,11 +22,47 @@ function tick() {
 
 document.getElementById("playButton").onclick = btnPlay;
 
+
+function initScene(){
+    let canvas = document.getElementById("canvas");
+    canvas.width = 1920;
+    canvas.height = 1080;
+    canvas.style.display = "none";
+
+    let context = canvas.getContext("2d");
+
+    let scene = new Scene(context);
+    let cannon = new Cannon(context, scene, 75, 650);
+    cannon.setController("player");
+    scene.addItem(cannon);
+
+    return scene;
+}
+
+function initBackground(){
+    let bg = document.getElementById("background");
+    bg.width = 1920;
+    bg.height = 1080;
+    bg.display = "";
+    let bgContext = bg.getContext("2d");
+    return new Background(bgContext);
+}
+
+function loadFont(){
+    let f = new FontFace('FredokaOne', 'url(./fonts/FredokaOne-Regular.ttf)');
+
+    f.load().then(function(font) {
+        console.log('Font ready: FredokaOne');
+        document.fonts.add(font);
+        context.font = '48px FredokaOne';
+    });
+}
+
 function btnPlay() {
     gameStarted = true;
-    background.style.display = "none";
+    document.getElementById("background").style.display = "none";
     document.getElementById("menu").style.display = "none";
-    canvas.style.display = "";
+    document.getElementById("canvas").style.display = "";
 }
 
 
