9cd2b792c3c3cd228818577d5d07b8cbf89bda13
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-12-19 15:24:23 +0300

    parse file

---
+++ parse.py
@@ -0,0 +1,30 @@
+file = open('tasks.csv', "r")
+lines = file.readlines()
+
+sequences = []
+sequencesArr = []
+
+for line in lines:
+    arr = line.replace("\n", "").split(";")
+
+    if len(arr) == 2:
+        if arr[1] == "new":
+            sequences.append({"id": -1, "sequence": sequencesArr})
+            sequencesArr = []
+
+        type = arr[1].replace("new", "")
+        if type == "":
+            scoreForHit = -50
+            scoreForNotHit = 30
+            typeArr = []
+        else:
+            scoreForHit = 30
+            scoreForNotHit = -50
+            typeArr = [type]
+
+        sequence = {"unicode": arr[0], "types": typeArr, "scoreForHit": scoreForHit, "scoreForSkip": scoreForNotHit}
+        sequencesArr.append(sequence)
+
+
+sequences.pop(0)
+print(sequences)
